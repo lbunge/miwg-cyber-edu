28faaa86c13f5da1a7d3037346b5faf07293e34f
luke <lukebunge@live.com>
2020-03-18 14:51:24 -0400

    updated paths

--- pullconfigoptions.bat
+++ pullconfigoptions.bat
@@ -1,15 +1,17 @@
-REM Updates script to modify VM
+REM Stored locally on VM to pull latest configuration options script
 SetLocal
 set homedir="C:\scripts"
 
+REM Creates scripts folder and downloads configoptions script
 if EXIST %homedir% (
-	curl -L https://raw.githubusercontent.com/lbunge/miwg-cyber-edu/master/options.bat -o "%homedir%\options.bat"
+	curl -L https://raw.githubusercontent.com/lbunge/miwg-cyber-edu/master/configoptions.bat -o "%homedir%\configoptions.bat"
 ) ELSE (
 	md %homedir%
-	curl -L https://raw.githubusercontent.com/lbunge/miwg-cyber-edu/master/options.bat -o "%homedir%\options.bat"
+	curl -L https://raw.githubusercontent.com/lbunge/miwg-cyber-edu/master/configoptions.bat -o "%homedir%\configoptions.bat"
 )
 
-"%homedir%\current\updateVM.bat"
+REM Runs configoptions script, will wait for it to finish
+"%homedir%\configoptions.bat"
 
 REM removes script directory to clean up
 rm -rf %homedir%
